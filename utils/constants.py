ef0c915677ea5734a855932296c3e889eb66eecf
JeidoUran <tourais.q@gmail.com>
2026-01-18 17:34:30 +0100

    Change bot picture to rounded one

--- utils/constants.py
+++ utils/constants.py
@@ -1,4 +1,4 @@
 SPHERE_NAME = "Project Sphere"
 SPHERE_VERSION = "1.6 Beta"
 SPHERE_MESSAGE = f"Running {SPHERE_NAME} v{SPHERE_VERSION}"
-SPHERE_THUMBNAIL = "https://s3.storage.memiroa.com/memiroa_icon.png"
+SPHERE_THUMBNAIL = "https://s3.storage.memiroa.com/memiroa_icon_circle.png"
